e354144e3bbfd5988fd20592748502eabb4565fe
Alexander <Alexander29_90@icloud.com>
2024-10-08 23:02:15 +0700

    Added Text and TextField to the LoginView screen

--- DemoExamTest/Core/LogIn/Views/LogInView.swift
+++ DemoExamTest/Core/LogIn/Views/LogInView.swift
@@ -8,8 +8,27 @@
 import SwiftUI
 
 struct LogInView: View {
+    
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack(alignment: .leading, spacing: 25) {
+            VStack(alignment: .leading, spacing: 5) {
+                Text("Welcome Back")
+                    .robotoFont(size: 24, weight: .medium)
+                    .foregroundStyle(.black)
+                Text("Fill in your email and password to continue")
+                    .robotoFont(size: 14, weight: .medium)
+                    .foregroundStyle(.gray)
+            }
+            
+            VStack {
+                CustomTextField(label: "Email Address", placeholder: "***********@mail.com", text: $email)
+                CustomTextField(label: "Password", placeholder: "**********", text: $password, isSecure: true)
+            }
+        }
     }
 }
 
