35fd30162119809292b5ab22ec5a39397667cf0f
Alexander <Alexander29_90@icloud.com>
2024-10-08 20:19:35 +0700

    Added a custom checkbox and implemented it on the Signap screen

--- DemoExamTest/Core/SignUp/Views/SignUpView.swift
+++ DemoExamTest/Core/SignUp/Views/SignUpView.swift
@@ -15,6 +15,8 @@ struct SignUpView: View {
     @State private var password = ""
     @State private var confirmPassword = ""
     
+    @State private var checkBox = false
+    
     var body: some View {
         ScrollView {
             VStack(alignment: .leading, spacing: 25) {
@@ -30,6 +32,16 @@ struct SignUpView: View {
                     CustomTextField(label: "Email Address", placeholder: "***********@mail.com", text: $email)
                     CustomTextField(label: "Password", placeholder: "**********", text: $password, isSecure: true)
                     CustomTextField(label: "Confirm Password", placeholder: "**********", text: $confirmPassword, isSecure: true)
+                }
+                
+                HStack(alignment: .top, spacing: 25) {
+                    CheckBox(value: $checkBox)
+                    
+                    Link(destination: URL(string: "https://developer.apple.com/")!) {
+                        Text("\(Text("By ticking this box, you agree to our").foregroundColor(.gray)) \(Text("Terms and conditions and private policy").foregroundColor(.yellow))")
+                            .robotoFont(size: 12)
+                    }
+
                 }
             }
         }
