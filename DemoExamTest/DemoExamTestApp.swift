c6694493236ebf8fe8befe5b23be615b9c9cc6a8
Alexander <Alexander29_90@icloud.com>
2024-10-08 18:45:28 +0700

    Initial Commit

---
+++ DemoExamTest/DemoExamTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoExamTestApp.swift
+//  DemoExamTest
+//
+//  Created by Александр on 08.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct DemoExamTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
